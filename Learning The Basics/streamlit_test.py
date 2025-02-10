2930b2d84b36a829aa22177d595320bc87aa1336
A-wase <abdulwase983@gmail.com>
2025-02-10 00:00:38 +0000

    added more lines to stream_lit test

--- Learning The Basics/streamlit_test.py
+++ Learning The Basics/streamlit_test.py
@@ -1,4 +1,15 @@
 # Paste in terminal: streamlit run streamlit_test.py
 import streamlit as st
 
-st.title("Data Dashboard") # This line creates the title
+st.title("Data Dashboard") # Create a title
+st.write("Hello, World!") # Create text
+
+import pandas as pd
+# Create a simple DataFrame
+data = {
+    'Name': ['Spongebob', 'Patrick', 'Squidward', 'Sandy'],
+    'Age': [24, 27, 22, 32],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+df = pd.DataFrame(data)
+st.dataframe(df)
